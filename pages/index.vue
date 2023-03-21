d40943f06ff9d91757e0b93eee483ed6b2d866be
ikun <0215.miyatto@gmail.com>
2023-03-21 10:32:35 +0900

    navbarの作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,8 @@
 <template>
-  <div>
-    <nuxt-link to="/products" class="a-button-buy-again">Add a new product</nuxt-link>
-    <Tutorial/>
-  </div>
+
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
 }
 </script>
