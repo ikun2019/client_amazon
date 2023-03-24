bdd97d227759d1b5ad40694c31532765f1435f41
ikun <0215.miyatto@gmail.com>
2023-03-25 08:49:02 +0900

    signupのUI作成

---
+++ layouts/none.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  body {
+    font-size: 13px;
+    line-height: 13px;
+    color: #111;
+  }
+</style>
